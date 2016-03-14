8afcde0ca7204c3145f2ac0d817ed903cc273f6f
Roger Tinsley <mail@rogertinsley.co.uk>
2016-03-14 17:34:14 +0000

    Project views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def github
+    unless @github.present?
+      token = session[:access_token]
+      @github = Octokit::Client.new access_token: token
+    end
+    @github
+  end
 end
