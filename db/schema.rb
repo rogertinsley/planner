8ba233cba5aef56da1837796e6a287fa01cce1e5
Roger Tinsley <mail@rogertinsley.co.uk>
2016-03-17 14:07:00 +0000

    Removed UID from users model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160315211059) do
+ActiveRecord::Schema.define(version: 20160317135611) do
 
   create_table "projects", force: :cascade do |t|
     t.datetime "created_at",        null: false
@@ -20,7 +20,6 @@ ActiveRecord::Schema.define(version: 20160315211059) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "uid"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "avatar_url"
