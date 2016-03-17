95bfca7f8df06844464ea0bbb06ec78393ee7296
Roger Tinsley <mail@rogertinsley.co.uk>
2016-03-17 14:47:27 +0000

    Display project name

--- app/controllers/backlog_controller.rb
+++ app/controllers/backlog_controller.rb
@@ -1,6 +1,13 @@
 class BacklogController < ApplicationController
 
+  before_action :set_project, only: [:show]
+
   def show
+  end
+
+  private
+  def set_project
+    @project = Project.find_by_github_repository(params[:id])
   end
 
 end
