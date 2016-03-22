8835fa57cae609d89888ad6a23a4b9b632891868
Roger Tinsley <mail@rogertinsley.co.uk>
2016-03-22 20:04:59 +0000

    Create milestone

--- app/controllers/milestones_controller.rb
+++ app/controllers/milestones_controller.rb
@@ -2,7 +2,8 @@ class MilestonesController < ApplicationController
 
   def create
     milestone = Milestone.new(milestone_params)
-    github_issue = current_user.github.create_milestone(current_repo, milestone.title, milestone.description, milestone.due_date)
+    options = { :state => 'open', :description => milestone.description, :due_on => Time.parse(milestone.due_date) }
+    github_issue = current_user.github.create_milestone(current_repo, milestone.title, options)
     redirect_to backlog_path
   end
 
