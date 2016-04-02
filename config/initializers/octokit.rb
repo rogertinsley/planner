eb8f11f36a2aeaebf016057e409507bd8d1a9c66
Roger Tinsley <mail@rogertinsley.co.uk>
2016-04-02 15:05:40 +0100

    Added comment

--- config/initializers/octokit.rb
+++ config/initializers/octokit.rb
@@ -1,4 +1,5 @@
 stack = Faraday::RackBuilder.new do |builder|
+  # shared_cache: false will cache private responses
   builder.use Faraday::HttpCache, shared_cache: false
   builder.use Octokit::Response::RaiseError
   builder.adapter Faraday.default_adapter
